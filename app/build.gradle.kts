ae212f48dbef9faa6a7967e1cd12e90642d6a2ef
Jogy <jogyfelix1@gmail.com>
2025-09-01 20:49:30 +0530

    chore: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 4
-        versionName = "0.4"
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
