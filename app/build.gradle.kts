25895363177b3c83244c009fd69f31a0d34b8373
Jogy <jogyfelix1@gmail.com>
2025-09-08 20:52:15 +0530

    build: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jolabs.looplog"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.5"
+        versionCode = 6
+        versionName = "0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
